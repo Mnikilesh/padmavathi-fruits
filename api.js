9e8192648ff02a1615c0a98ca7b375cc817cd689
M Nikilesh <nikileshmamidala@gmail.com>
2026-04-15 18:13:57 +0530

    fix api prefix for render

--- api.js
+++ api.js
@@ -1574,7 +1574,7 @@ const server = http.createServer(async (req, res) => {
     await connectDB();
 
     const url = new URL(req.url, `http://${req.headers.host}`);
-    const path = url.pathname;
+    const path = url.pathname.replace(/^\/api/, '') || '/';
     const method = req.method;
 
     let body = '';
